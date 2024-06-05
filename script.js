9068b177398fc30a2b818de4b3a063c1dfd511a7
MattGitHub01 <tuckermatthewcrandall@gmail.com>
2024-06-05 12:17:13 -0600

    Create html css and js file. Add basic html and css custom font

---
+++ script.js
@@ -0,0 +1,12 @@
+const library = [];
+
+function Book(name, author, pages, id) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.id = id;
+};
+
+function addNewBook() {
+
+};
