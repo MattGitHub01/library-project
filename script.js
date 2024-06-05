fd6c8471ce464a3654299c46e9e2d5a2ef2b9746
MattGitHub01 <tuckermatthewcrandall@gmail.com>
2024-06-05 13:56:17 -0600

    Create add book form and site logo

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const library = [];
-
 function Book(name, author, pages, id) {
     this.name = name;
     this.author = author;
@@ -7,6 +5,45 @@ function Book(name, author, pages, id) {
     this.id = id;
 };
 
-function addNewBook() {
+const library = [];
 
+const testBookOne = {
+    name: 'Test',
+    author: 'Dave Dave',
+    pages: 100,
+    id: 1
 };
+
+function addNewBook() {
+    
+    library.push(testBookOne);
+};
+
+addNewBook()
+
+const container = document.querySelector('.container');
+
+const card = document.createElement('div');
+card.classList = 'card';
+container.appendChild(card);
+
+const title = document.createElement('p');
+title.textContent = `Name: ${library[0].name}\n`;
+card.appendChild(title);
+
+const author = document.createElement('p');
+author.textContent = `Author: ${library[0].author}\n`;
+card.appendChild(author);
+
+const pages = document.createElement('p');
+pages.textContent = `Pages: ${library[0].pages}`;
+card.appendChild(pages);
+
+const button = document.createElement('button');
+button.classList = 'new-book';
+button.textContent = 'Add New Book';
+button.addEventListener('click', () => {
+    const newBookForm = document.querySelector('.add-book-form');
+    newBookForm.showModal();
+});
+container.appendChild(button);
